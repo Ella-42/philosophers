e2ef32227ffd6b1c4a63d0ce7ea663fa1c2a494d
LarsCampus19 <Larsp.jobs@gmail.com>
2023-08-16 23:21:31 +0200

    just fucked around tbh, not much time left after comany visit(auto_push)

--- philo/philo.c
+++ philo/philo.c
@@ -6,15 +6,23 @@
 /*   By: lpeeters <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 21:23:51 by lpeeters          #+#    #+#             */
-/*   Updated: 2023/08/15 16:08:17 by lpeeters         ###   ########.fr       */
+/*   Updated: 2023/08/16 22:48:28 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+void	error(int type)
+{
+	if (type == ARGUMENT)
+		printf("Error:\nInvalid amount of arguments\n");
+	exit(type);
+}
+
 int	main(int ac, char **av)
 {
 	(void)av;
 	if (5 > ac || ac > 6)
-		return (1);
+		error(ARGUMENT);
+	return (2147483392);
 }
--- philo/philo.h
+++ philo/philo.h
@@ -6,11 +6,22 @@
 /*   By: lpeeters <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 21:25:21 by lpeeters          #+#    #+#             */
-/*   Updated: 2023/08/15 16:08:13 by lpeeters         ###   ########.fr       */
+/*   Updated: 2023/08/16 21:35:20 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+
+/*libraries*/
+
+//exit
+# include <stdlib.h>
+
+//printf
+# include <stdio.h>
+
+//macros
+# define ARGUMENT 22
 
 #endif
