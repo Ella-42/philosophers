9a038bf6f828f4f87e1c532cdff5f2791b71b826
LarsCampus19 <Larsp.jobs@gmail.com>
2023-08-17 22:48:53 +0200

    added micro_atoi to convert inputs from strings into ints, also worked on thinking of how to store data, likely going to go for struct arrays. Couldn't do much bcs of exam, passed with 100100(auto_push)

--- philo/philo.c
+++ philo/philo.c
@@ -6,12 +6,24 @@
 /*   By: lpeeters <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/14 21:23:51 by lpeeters          #+#    #+#             */
-/*   Updated: 2023/08/16 22:48:28 by lpeeters         ###   ########.fr       */
+/*   Updated: 2023/08/17 22:40:01 by lpeeters         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+//very basic string to number conversion
+int	micro_atoi(char *str)
+{
+	int	nb;
+
+	nb = 0;
+	while (*str != '\0')
+		nb = nb * 10 + (*str++ - 48);
+	return (nb);
+}
+
+//error handler
 void	error(int type)
 {
 	if (type == ARGUMENT)
@@ -19,10 +31,13 @@ void	error(int type)
 	exit(type);
 }
 
+//create threads, mutexes and run a simulation to test these
 int	main(int ac, char **av)
 {
-	(void)av;
+	int	p_nb;
+
 	if (5 > ac || ac > 6)
 		error(ARGUMENT);
-	return (2147483392);
+	p_nb = micro_atoi(av[1]);
+	printf("%i philosophers\n", p_nb);
 }
